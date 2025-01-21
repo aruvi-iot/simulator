fded4d7aa18a149cf0f6225b4b6cbc1e135bc79e
aruvi-iot <aruvib@creamcollar.com>
2025-01-21 16:27:51 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+ESP8266_IoT.connectWifi("Testing", "123456789")
+OLED.init(128, 64)
+for (let index = 0; index < 4; index++) {
+	
+}
+dht11_dht22.queryData(
+DHTtype.DHT11,
+DigitalPin.P3,
+true,
+false,
+true
+)
+basic.forever(function () {
+    basic.showString("")
+})
